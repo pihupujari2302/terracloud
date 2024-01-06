3b0bfc5298cf2e6e412c1abb4ba5d25e8c311f3d
pihupujari2302 <155809210+pihupujari2302@users.noreply.github.com>
2024-01-06 14:43:48 +0530

    Update mian.tf

--- mian.tf
+++ mian.tf
@@ -17,7 +17,8 @@ resource "aws_instance" "myec2" {
   ami           = "ami-03a6eaae9938c858c"
   instance_type = "t2.micro"
   vpc_security_group_ids=[aws_security_group.web-sg.id]
-  key_name="tf-key-pair"  tags={
+  key_name="tf-key-pair" 
+tags={
  Name="web-server"
 }
 user_data= <<-EOF
